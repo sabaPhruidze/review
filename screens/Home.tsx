eca19d176a646a17abca91ed951fcf4d314c6850
Saba Phruidze <sabaphruidze01@gmail.com>
2023-12-03 14:56:59 +0400

    I am going to learn how to pass data between the screens

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,8 +1,34 @@
 import {View, Text, Button} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {globalStyles} from '../styles/Global';
 
 const Home = ({navigation}: {navigation: any}) => {
+  interface Review {
+    title: string;
+    rating: number;
+    body: string;
+    key: number;
+  }
+  const [reviews, setReviews] = useState<Review[]>([
+    {
+      title: 'Zelda Breath of Fresh Air',
+      rating: 5,
+      body: 'lorem ipsum',
+      key: 1,
+    },
+    {
+      title: 'Gotta Catch Them All (Again)',
+      rating: 4,
+      body: 'lorem ipsum',
+      key: 2,
+    },
+    {
+      title: 'Not So "Final" Fantasy',
+      rating: 3,
+      body: 'lorem ipsum',
+      key: 3,
+    },
+  ]);
   const pressHandler = () => {
     navigation.push('About');
   };
