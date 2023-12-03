9d97d52e2904ba2085bfd8b1376f082dc84b3070
Saba Phruidze <sabaphruidze01@gmail.com>
2023-12-03 15:18:47 +0400

    I received specific information when I click concrete title

--- screens/ReviewDetails.tsx
+++ screens/ReviewDetails.tsx
@@ -1,14 +1,16 @@
 import {View, Text, Button} from 'react-native';
 import React from 'react';
 import {globalStyles} from '../styles/Global';
-const ReviewDetails = ({navigation}: {navigation: any}) => {
-  const pressHandler = () => {
-    navigation.goBack();
-  };
+const ReviewDetails = ({navigation, route}: {navigation: any; route: any}) => {
+  // const pressHandler = () => {
+  //   navigation.goBack();
+  // };
   return (
     <View style={globalStyles.container}>
-      <Text>Review details</Text>
-      <Button title="go back to About page" onPress={pressHandler} />
+      <Text>{route.params.title}</Text>
+      <Text>{route.params.body}</Text>
+      <Text>{route.params.rating}</Text>
+      {/* <Button title="go back to About page" onPress={pressHandler} /> */}
     </View>
   );
 };
